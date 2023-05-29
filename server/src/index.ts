d477884c3898cfe74f937bfd766b0bc6cb5c283f
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-05-28 23:57:33 -0700

    Update function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
-export const handler = async (): Promise<any> => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('The GitHub Action successfully deployed to AWS!'),
-    };
-    return response;
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
+
+export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("The GitHub Action successfully deployed to AWS!"),
   };
+  return response;
+};

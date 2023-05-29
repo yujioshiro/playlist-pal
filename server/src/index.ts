134cd18aeaf9fb361ba898f1d30fa5c9ca7f119c
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-05-28 23:36:13 -0700

    Update function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 export const handler = async (): Promise<any> => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify('The GitHub Action successfully deployed to AWS!'),
+      body: JSON.stringify('The GitHub Action successfully deployed to AWS!!'),
     };
     return response;
   };

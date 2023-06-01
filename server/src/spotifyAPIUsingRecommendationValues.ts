3b79b8fc9868eef50450acc92f769b03b50e51aa
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-06-01 14:44:00 -0700

    Update function

--- server/src/spotifyAPIUsingRecommendationValues.ts
+++ server/src/spotifyAPIUsingRecommendationValues.ts
@@ -137,13 +137,14 @@ export async function createPlaylistAndAddSongsWithValues ( valuesObject: any, i
         body: `grant_type=refresh_token&refresh_token=${encodeURIComponent(refreshToken)}`,
     });
 
-    // console.log("retrieving accessToken JSON Data Response");
+    console.log("retrieving accessToken JSON Data Response");
     const accessTokenWithPublicScopeResponseData = await accessTokenWithPublicScopeResponse.json();
-    // console.log("Retrieved data response");
+    console.log("Retrieved data response");
     const accessToken = accessTokenWithPublicScopeResponseData.access_token;
-    // console.log("Retrieved Access Token:", accessToken);
+    console.log("Retrieved Access Token:", accessToken);
 
     const AVAILABLE_GENRES = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"]
+    
     // search for genres
     for (let i = 0; i < valuesInJSON.genres.length; i++) {
         valuesInJSON.genres[i] = valuesInJSON.genres[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ' ');
@@ -166,6 +167,7 @@ export async function createPlaylistAndAddSongsWithValues ( valuesObject: any, i
 
     // search for artist IDs
     for (let i = 0; i < valuesInJSON.artists.length; i++) {
+      console.log("Searching for artist ID: ",valuesInJSON.artists[i]);
 
         const searchResponse = await fetch(`https://api.spotify.com/v1/search?q=${valuesInJSON.artists[i]}&type=artist`, {
         headers: {

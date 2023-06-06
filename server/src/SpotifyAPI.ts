05baf2697b6327c0ffbaa42bda07139d2d83cafe
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-06-05 19:08:25 -0700

    Fix addSongsToPlaylist

--- server/src/SpotifyAPI.ts
+++ server/src/SpotifyAPI.ts
@@ -156,8 +156,8 @@ export async function createPlaylist(prompt: string, songs: string[], imageObjec
 }
 
 export async function addSongsToPlaylist(songs: string[], playlistId: string, accessToken: string) {
-    for (let song of songs) {
-        // console.log(`song: ${song}`);
+    for (let song in songs) {
+        console.log(`song: ${song}`);
         await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=spotify:track:${song}`, {
             method: "POST",
             headers: {

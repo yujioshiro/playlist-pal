81b7d2c727ea6a543dcb09ff8e0a68f5df49b8cf
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-06-09 14:04:22 -0700

    Reduced API calls

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { getInitialSongsFromOpenAiChatCompletion, getFourMatchingSongsFromOpenAiChatCompletion, getImageFromOpenAi } from './OpenAIAPI';
-import { getSongIds, getSongRecommendations, createPlaylist } from './SpotifyAPI';
+import { getAccessToken, getSongIds, getSongRecommendations, createPlaylist } from './SpotifyAPI';
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
     console.log(event.path);
@@ -14,22 +14,27 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
                 'Access-Control-Allow-Headers': 'Content-Type',
             },
             body: JSON.stringify({
-                output: await secondHandler(JSON.parse(event.body!).prompt, JSON.parse(event.body!).song),
+                output: await secondHandler(JSON.parse(event.body!).prompt, JSON.parse(event.body!).song, JSON.parse(event.body!).accessToken),
             })
         }
     }
 
     let prompt: string;
+    let accessToken: string;
 
     // The prompt is parsed and checked for any errors
     // Errors might include blank prompts (which is checked on the client side, but should also be checked here)
     try {
-
         // When user submits prompt, we parse prompt into a string to send to OpenAI
         if (event.body && event.body !== null) {
             // console.log(event.body);
             const requestBody = JSON.parse(event.body);
             prompt = requestBody.prompt;
+            accessToken = requestBody.accessToken
+
+            if (accessToken === 'NEEDS TOKEN') {
+                accessToken = await getAccessToken();
+            }
 
             // GPT-4 will generate 10 songs of varying genres based on that one prompt. These 10 songs are returned to the user so they can select the genre that fits their mood the most
             console.info(`Prompt within index.ts received from App.tsx: ${prompt}`);
@@ -37,7 +42,7 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
             // let initialSongs = JSON.parse('[{"artist":"The Beatles","song":"Here Comes The Sun"},{"artist":"Frank Sinatra","song":"Fly Me To The Moon"},{"artist":"Ella Fitzgerald","song":"Blue Skies"},{"artist":"Israel Kamakawiwoole","song":"Somewhere Over The Rainbow"},{"artist":"Norah Jones","song":"Sunrise"},{"artist":"Coldplay","song":"Viva La Vida"},{"artist":"Johnny Cash","song":"I Walk The Line"},{"artist":"Bob Marley","song":"Three Little Birds"},{"artist":"James Brown","song":"I Got You (I Feel Good)"},{"artist":"John Denver","song":"Take Me Home, Country Roads"}]')
             console.log(`initialSongs: ${initialSongs}`);
             console.log(`initialSongs Stringified: ${JSON.stringify(initialSongs)}`);
-            let songIds = JSON.parse((await getSongIds(initialSongs)).body);
+            let songIds = JSON.parse((await getSongIds(initialSongs, accessToken)).body);
             songIds.message = JSON.stringify(songIds.message)
             console.log(`Result of GetSongIds() at index.handler: ${JSON.stringify(songIds)}`);
             return {
@@ -48,6 +53,7 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
                   },
                 body: JSON.stringify({
                     output: songIds.message,
+                    accessToken: accessToken
                 })
             }
         } else {
@@ -77,7 +83,7 @@ type PlaylistToBuild = {
     tracks: string[];
 }
 
-export async function secondHandler(prompt: string, selectedSong: Track) {
+export async function secondHandler(prompt: string, selectedSong: Track, accessToken: string) {
     console.log(`*******************************************************`);
     console.log(`*******************************************************`);
     console.log(`*******************************************************`);
@@ -85,6 +91,7 @@ export async function secondHandler(prompt: string, selectedSong: Track) {
     console.log(`*******************************************************`);
     console.log(`*******************************************************`);
     console.log(`*******************************************************`);
+    console.log(accessToken);
 
     let playlist: PlaylistToBuild = {tracks:[]}
 
@@ -99,17 +106,17 @@ export async function secondHandler(prompt: string, selectedSong: Track) {
     // console.log(`SELECTED SONG: ${JSON.stringify(selectedSong)}`);
     let songsToPassToRecommendationFunction = JSON.parse(JSON.parse((await getFourMatchingSongsFromOpenAiChatCompletion(prompt, artist, song)).result))
     // let songsToPassToRecommendationFunction = JSON.parse('[{"artist":"Jack Johnson","song":"Banana Pancakes"},{"artist":"Corinne Bailey Rae","song":"Put Your Records On"},{"artist":"Jason Mraz","song":"Im Yours"},{"artist":"Sara Bareilles","song":"Love Song"}]')
-    let songIds = JSON.parse((await getSongIds(songsToPassToRecommendationFunction)).body)
+    let songIds = JSON.parse((await getSongIds(songsToPassToRecommendationFunction, accessToken)).body)
     songIds.message.push(selectedSong)
     // console.log(`Result of GetSongIds() at index.secondHandler: ${JSON.stringify(songIds)}`);
 
     // The 5 songs are sent to Spotify Web API to get 100 song recommendations for each
     console.log('running the getSongsRecommendations function');
     console.log(`songIds.message: ${JSON.stringify(songIds.message)}`);
-    let songRecs: string[] = (JSON.parse((await getSongRecommendations(songIds.message)).body)).message
+    let songRecs: string[] = (JSON.parse((await getSongRecommendations(songIds.message, accessToken)).body)).message
 
     // Once the playlist has at least 30 songs (but under 40), it is built with Spotify and the playlist ID is returned to the client. The app should check that there are at least 30 songs before sending the ID.
-    let playlistId = await createPlaylist(prompt, songRecs, await getImageFromOpenAi(prompt))
+    let playlistId = await createPlaylist(prompt, songRecs, await getImageFromOpenAi(prompt), accessToken)
 
     console.log(typeof playlistId.result);
     console.log(playlistId);

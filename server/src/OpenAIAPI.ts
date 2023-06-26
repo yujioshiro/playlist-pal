7f07e1c6df5edf6fdbee900f0fba229bfc4d6027
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-06-26 12:58:48 -0700

    Change prompt wording

--- server/src/OpenAIAPI.ts
+++ server/src/OpenAIAPI.ts
@@ -15,7 +15,7 @@ export async function getInitialSongsFromOpenAiChatCompletion(prompt: string): P
         console.log(`Getting initial songs from GPT using ${prompt}`);
         const completion = await openai.createChatCompletion({
             model: "gpt-4",
-            messages: [{role:'user', content:`Give 7 songs from different genres that match this prompt: ${prompt}.Put results in JSON and omit all other text.Example: [{"artist":"artist name","song":"song name"},{"artist":"artist name","song":"song name"},{"artist":"artist name","song":"song name"}]`}],
+            messages: [{role:'user', content:`You are the world's best music curator. give me 7 songs that sound like ${prompt}.Results in JSON,omit all other text.Example: [{"artist":"artist name","song":"song name"},{"artist":"artist name","song":"song name"},{"artist":"artist name","song":"song name"}]`}],
             temperature: 0.7,
             max_tokens: 750,
         })
